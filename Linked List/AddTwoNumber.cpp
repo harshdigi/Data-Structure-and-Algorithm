14f9183c24faf93a49f336d27ed809e712d85e30
harsgdigi <harshkahuja@gmail.com>
2021-08-30 12:53:24 +0530

    Add Two Number Linked List

---
+++ Linked List/AddTwoNumber.cpp
@@ -0,0 +1,68 @@
+/*You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order, and each of their nodes contains a single digit. 
+Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example:
+2 -> 4 -> 3 
+5 -> 6 -> 4
+------------
+7 -> 0 -> 8
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+     ListNode* temphead=new ListNode(-1);
+        ListNode* node=temphead;
+        int carry=0;
+        
+        while(l1!=NULL || l2!=NULL)
+        {
+            int sum=carry;
+            if(l1!=NULL)
+            {
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2!=NULL)
+            {
+                sum+=l2->val;
+                l2=l2->next;
+            }
+            node->next=new ListNode(sum%10);
+            node=node->next;
+            carry=sum/10;
+            
+            
+            
+        }
+        if(carry!=0)
+            node->next=new ListNode(carry);
+        
+        return temphead->next;
+    }
+};
